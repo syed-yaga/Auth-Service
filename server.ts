3e84b6f4b498a80bb15c20281d36483f218fbd39
Syed Saqib <soharwardiandco27@gmail.com>
2026-03-16 03:00:46 +0530

    updated server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app";
 
-const PORT = 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
